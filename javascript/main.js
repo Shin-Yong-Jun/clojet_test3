0cdfddcaa984d7b527e54c65274a0b5de95fddd9
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-02-15 23:41:13 +0900

    검색창과 백스크롤 오류 해결 (백스크롤에 필요한 태그를 헤더에 넣었는데 백스크롤이 가장 상단에 동작해야 가능)

--- javascript/main.js
+++ javascript/main.js
@@ -1,48 +1,53 @@
 'use strict';
 
+//====== 백스크롤 버튼 ====
+const header = document.querySelector('header'); 
+header.innerHTML += '<div class="btn_scrollBack"><img src = "../../image/backscroll.png" </div>' ;
+const btn_scrollBack = document.querySelector('.btn_scrollBack');
+
+
+
+window.addEventListener('scroll', () => {
+if(window.scrollY > window.innerHeight / 3) {
+  btn_scrollBack.style.opacity = '1';
+  btn_scrollBack.style.visibility = 'visible';
+} else {
+  btn_scrollBack.style.opacity = '0';
+  btn_scrollBack.style.visibility = 'hidden';
+}
+})
+
+btn_scrollBack.addEventListener('click', () => {
+const stopScroll = setInterval(() => {
+  window.scrollBy(0,-30);
+
+  if(window.scrollY <= 0) clearInterval(stopScroll);
+})
+});
+
 //====== 검색창 등장  ====
 const search = document.querySelector('.search'),
   search_symbol = search.querySelector('span'),
   search_input = search.querySelector('input');
 
-let clickIdx = 0
-search_symbol.addEventListener('click', () => {
-  clickIdx = clickIdx + 1;
-  search_input.style.opacity = '1';
-  search_input.style.visibility = 'visible';
-  search_input.style.width = '180px'
 
-  if (clickIdx % 2 === 0) {
-    search_input.style.opacity = '0';
-    search_input.style.visibility = 'hidden';
-    search_input.style.width = '0px'
-  }
-});
+  let clickIdx = 0
 
+  search_symbol.addEventListener('click', (e) => {
+    e.preventDefault();
 
-//====== 백스크롤 버튼 ====
-const header = document.querySelector('header'); 
-header.innerHTML += '<div class="btn_scrollBack"><img src = "../../image/backscroll.png" </div>'
-const btn_scrollBack = header.querySelector('.btn_scrollBack');
-
-
-
-window.addEventListener('scroll', () => {
-  if(window.scrollY > window.innerHeight / 3) {
-    btn_scrollBack.style.opacity = '1';
-    btn_scrollBack.style.visibility = 'visible';
-  } else {
-    btn_scrollBack.style.opacity = '0';
-    btn_scrollBack.style.visibility = 'hidden';
-  }
-})
-
-btn_scrollBack.addEventListener('click', () => {
-  const stopScroll = setInterval(() => {
-    window.scrollBy(0,-30);
-
-    if(window.scrollY <= 0) clearInterval(stopScroll);
-  })
-});
+    clickIdx = clickIdx + 1;
+    search_input.style.opacity = '1';
+    search_input.style.visibility = 'visible';
+    search_input.style.width = '180px'
+    
+    if (clickIdx % 2 === 0) {
+      search_input.style.opacity = '0';
+      search_input.style.visibility = 'hidden';
+      search_input.style.width = '0px'
+    }
+  });
+  
+  
 
 
