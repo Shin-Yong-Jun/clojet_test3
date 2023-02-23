093fba8557fce4167ef229b8f0f451111b6d6b9f
inku <necron7577@daum.net>
2023-02-23 16:11:52 +0900

    디테일수정

---
+++ javascript/data.js
@@ -0,0 +1,34 @@
+// 상품정보
+const info = [
+    {
+        title: '캐시미어 코트 유로컬렉션 22FW 쉬어드 리얼밍크 케이프 자켓',
+        subtitle: '유로컬렉션',
+        price: 3000000,
+        buy_price: 2850000,
+        discount: 5,
+        size: ['90', '95', '100', '105'],
+        color: ['gray'],
+        sex: 'woman',
+        thum_imgAr: [
+            '../detail/img/Preview/0.jpg',
+            '../detail/img/Preview/1.jpg',
+            '../detail/img/Preview/2.jpg',
+            '../detail/img/Preview/3.jpg',
+            '../detail/img/Preview/4.jpg',
+            '../detail/img/Preview/5.jpg',
+            '../detail/img/Preview/6.jpg',
+        ],
+    },
+    {
+        title: '2번상품',
+        subtitle: '2번상품_서브',
+        price: 500,
+        buy_price: 90,
+        discount: 5,
+        size: ['90', '95', '100', '105'],
+        color: ['gray'],
+        sex: 'man',
+    },
+];
+
+export default info;
