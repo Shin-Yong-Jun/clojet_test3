4995293cf75c9f00f0e69e177e6fc8749fff861d
inku <necron7577@daum.net>
2023-02-28 13:00:49 +0900

    카테고리 페이지 // 색상 기능 추가 , 사이즈 버튼추가

--- html/category/javascript/category.js
+++ html/category/javascript/category.js
@@ -1,18 +1,15 @@
 import info from './data.js';
 ('use strict');
-/****************************************************************************/
 
-const main = document.querySelector('main'),
-    product_list_ul = main.querySelector('.product_list_ul');
+const main = document.querySelector('main');
+/****************************************************************************/
+// 상품 만들기
+const product_list_ul = main.querySelector('.product_list_ul');
 
 function show_list(search = '') {
-    product_list_ul.innerHTML = '';
-
     info.forEach((value, index) => {
-        const colorAr = value.color;
-        colorAr.forEach((colorArr) => {
-            if (value.color.includes(search)) {
-                product_list_ul.innerHTML += `
+        if (search === '' || value.color.includes(search)) {
+            product_list_ul.innerHTML += `
             <li class="product_list_li">
                 <div class="item_img">
                     <a href="./detail_page.html">
@@ -24,9 +21,6 @@ function show_list(search = '') {
                         src=${value.thum_imgAr[0]}
                             alt="thum"
                         />
-                        <span class="material-symbols-outlined"
-                            >favorite</span
-                        >
                     </a>
                 </div>
 
@@ -48,8 +42,52 @@ function show_list(search = '') {
                 </div>
             </li>
         `;
-            }
-        });
+        }
     });
 }
-show_list('gray');
+show_list();
+
+/****************************************************************************/
+// 상품 리스트 수량 체크
+function show_product_list_li_length() {
+    const product_list_li =
+            product_list_ul.querySelectorAll('.product_list_li'),
+        product_list_li_length = main.querySelector('.product_list_li_length');
+
+    product_list_li_length.textContent = `${product_list_li.length}개의 상품`;
+}
+show_product_list_li_length();
+
+/****************************************************************************/
+// 상품 컬러 클릭 이벤트
+const color = main.querySelector('.color'),
+    side_bar_color = color.querySelector('.side_bar_color'),
+    side_bar_color_li = side_bar_color.querySelectorAll('li');
+let colorArrrr = [];
+
+side_bar_color.addEventListener('click', (e) => {
+    function show_color(filter_target, aRpush) {
+        colorArrrr = colorArrrr.filter((value) => value != filter_target);
+        product_list_ul.innerHTML = '';
+        targetE.classList.toggle('checked');
+        aRpush;
+        colorArrrr.forEach((value) => {
+            show_list(value);
+            show_product_list_li_length();
+        });
+    }
+
+    let targetE = e.target.closest('.color li');
+
+    if (!targetE) return;
+    if (targetE.classList.contains('checked')) {
+        if (colorArrrr.length <= 1) {
+            colorArrrr = [''];
+        }
+        // 공통
+        show_color(targetE.dataset.color);
+    } else {
+        // 공통
+        show_color([''], colorArrrr.push(targetE.dataset.color));
+    }
+});
