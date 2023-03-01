028566581e9e90db0c2bb7bd27301a6dfede30ae
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2023-03-01 16:32:28 +0900

    경로설정 및 아이디찾기 페이지 작업완료

---
+++ html/login/javascript/findId.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const rad_findIdEmail = document.getElementById('rad_findIdEmail');
+const rad_findIdphone = document.getElementById('rad_findIdphone');
+const findId_input = document.querySelector('.findId_input');
+const findId_input_phoneNum = document.querySelector('.findId_input_phoneNum');
+
+findId_input_phoneNum.style.display = 'none';
+
+rad_findIdEmail.onclick = function() {
+  showInput('findId_input');
+}
+
+rad_findIdphone.onclick = function() {
+  showInput('findId_input_phoneNum');
+}
+
+function showInput(inputId) {
+  if (inputId === 'findId_input') {
+    findId_input.style.display = 'block';
+    findId_input_phoneNum.style.display = 'none';
+  } else if (inputId === 'findId_input_phoneNum') {
+    findId_input.style.display = 'none';
+    findId_input_phoneNum.style.display = 'block';
+  }
+}
--- html/mypage/javascript/mypage.js
+++ html/mypage/javascript/mypage.js
@@ -2,21 +2,39 @@
 
 function btnMove(targetBtn) {
 
-  if(targetBtn.className === 'btn1_foward')
+  if(targetBtn.className === 'btn1_foward'){
     wishlist_ul.style.left = '-102%'
-  if(targetBtn.className === 'btn1_back')
+    btn1_back.style.visibility = 'visible';
+    btn1_foward.style.visibility = 'hidden';
+  }
+  else if(targetBtn.className === 'btn1_back') {
     wishlist_ul.style.left = '0%'
-  if(targetBtn.className === 'btn2_foward')
+    btn1_back.style.visibility = 'hidden';
+    btn1_foward.style.visibility = 'visible';
+  }
+  else if(targetBtn.className === 'btn2_foward') {
     recent_ul.style.left = '-102%'
-  if(targetBtn.className === 'btn2_back')
+    btn2_back.style.visibility = 'visible';
+    btn2_foward.style.visibility = 'hidden';
+  }
+  else if(targetBtn.className === 'btn2_back') {
     recent_ul.style.left = '0%'
+    btn2_back.style.visibility = 'hidden';
+    btn2_foward.style.visibility = 'visible';
+  }
 }
 
 
 const my_page = document.querySelector('.my_page'),
 wishlist_ul = my_page.querySelector('.mybox4ul'),
-recent_ul = my_page.querySelector('.mybox5ul');
+recent_ul = my_page.querySelector('.mybox5ul'),
+btn1_back = my_page.querySelector('.btn1_back'),
+btn1_foward = my_page.querySelector('.btn1_foward'),
+btn2_back = my_page.querySelector('.btn2_back'),
+btn2_foward = my_page.querySelector('.btn2_foward');
 
+btn1_back.style.visibility = 'hidden';
+btn2_back.style.visibility = 'hidden';
 
 my_page.addEventListener('click', (e) => {
   let targetBtn = e.target.closest('a');
